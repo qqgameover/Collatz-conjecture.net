44887778925b6bc0918be17e0f9c849085e9402b
qqgameover <kasper35@live.no>
2021-09-09 13:03:29 +0200

    fin.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Collatz conjecture - Type any positive intiger.");
+            string intAsString = Console.ReadLine();
+            int number = int.Parse(intAsString);
+            CollatzConjecture(number);
+        }
+        static void CollatzConjecture(int n, int steps = 0)
+        {
+            if(n == 1)
+            {
+                Console.WriteLine($"It took {steps} steps to reach 1");
+            }
+            if(n % 2 == 0)
+            {
+                int nDivided = n / 2;
+                int step = steps + 1;
+                Console.WriteLine(nDivided);
+                CollatzConjecture(nDivided, step);
+            }
+            else if(n != 1)
+            {
+                int threeNplusOne = (n * 3) + 1;
+                int step = steps + 1;
+                Console.WriteLine(threeNplusOne);
+                CollatzConjecture(threeNplusOne, step);
+            }
+        }
+    }
+}
